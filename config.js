7e4c05e5a7ada80a6b1c7307db84901a80f7d93e
Morsdeimos <longden.jonathanedgar@gmail.com>
2016-06-17 11:13:39 -0600

    third time trying to heroku

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
-	port: process.env.port || 3000,
+	port: process.env.PORT || 3000,
 	mongo_uri: process.env.MONGOLAB_URI
 };
+
 
 // mongodb://<dbuser>:<dbpassword>@ds017544.mlab.com:17544/bigjonblog
